c26fb6ad527bdf3c705a82fdae43026b90d4839b
Harivardhan <kanthala.hari.198@gmail.com>
2021-10-31 22:02:04 +0530

    created react app

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    
+    return (
+        <div className="row">
+        <div className="col-2 text-center d-flex align-items-center justify-content-center"   >
+            <h1>Hello! Welcome to our WEB APP</h1>
+        </div>
+        </div>
+    )
+}
+
+export default Home
